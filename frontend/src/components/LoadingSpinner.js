a97b1054932d17c6bc162f160d6bfa22fcb63e63
Divya <divyadli201@gmail.com>
2025-04-21 14:02:58 -0700

    Updated loader

--- frontend/src/components/LoadingSpinner.js
+++ frontend/src/components/LoadingSpinner.js
@@ -1,18 +1,5 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import { FaWater } from "react-icons/fa";
-
-const wave = keyframes`
-  0% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-15px);
-  }
-  100% {
-    transform: translateY(0);
-  }
-`;
 
 const rotate = keyframes`
   from {
@@ -37,30 +24,6 @@ const SpinnerContainer = styled.div`
   width: 80px;
   height: 80px;
   margin-bottom: 1.5rem;
-`;
-
-// Add a background circle to make the wave icon stand out better
-const IconBackground = styled.div`
-  position: absolute;
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  background-color: white;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 5;
-`;
-
-const WaveIcon = styled(FaWater)`
-  color: ${(props) => props.theme.colors.teal};
-  font-size: 2rem;
-  animation: ${wave} 1.5s ease-in-out infinite;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 10; // Ensure it appears on top of the circles
 `;
 
 const Circle = styled.div`
@@ -107,8 +70,6 @@ const LoadingSpinner = ({
         <Circle />
         <Circle />
         <Circle />
-        <IconBackground />
-        <WaveIcon />
       </SpinnerContainer>
       <LoadingText>{message}</LoadingText>
       {subMessage && <SubText>{subMessage}</SubText>}
